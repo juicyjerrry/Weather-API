2822cf231156b17b012e91d7690f8488ee24b86d
Jeremiah Kendl <kendljeremiah@gmail.com>
2022-08-23 18:38:56 -0500

    Second Attempt
    Added date and working api

--- script.js
+++ script.js
@@ -2,40 +2,112 @@ var temp = document.querySelector("#temp");
 var wind = document.querySelector("#wind");
 var humidity = document.querySelector("#humidity");
 var UV = document.querySelector("#index");
+var searchButton = document.querySelector("#searchButton")
+var cityName = document.querySelector("#cityName")
 
-var lat = document.querySelector()
+var searchHistory1 = document.querySelector("searchHistory1")
+var searchHistory2 = document.querySelector("searchHistory2")
+var searchHistory3 = document.querySelector("searchHistory3")
+var searchHistory4 = document.querySelector("searchHistory4")
+var searchHistory5 = document.querySelector("searchHistory5")
+var searchHistory6 = document.querySelector("searchHistory6")
+var searchHistory7 = document.querySelector("searchHistory7")
+var searchHistory8 = document.querySelector("searchHistory8")
 
-var userInput = document.querySelector
 
-// var t = ;
-// var w = ;
-// var h = ;
+var forcast = document.querySelector("#forcast")
+
+var userInput = document.querySelector("#citySearch")
+
+const lat = localStorage.getItem('lat')
+const lon = localStorage.getItem('lon')
+
+
+var t = "";
+var w = "";
+var h = "";
 // var UVindex = ;
+// UV.textContent = "UV Index: " + UVindex
 
-temp.textContent = "Temperature: " + t + " °F"
-wind.textContent = "Wind: " + w + " MPH"
-humidity.textContent = "Humidity: " + h + "%"
-UV.textContent = "UV Index: " + UVindex
+//
+searchButton.addEventListener("click", function(){
+    var citySearch = userInput.value
+    oneCallApi(citySearch);
+    twoCallApi(lat, lon);
+})
 
 //💾 api goes here
-//confused on how to translate user input to lat/long
-var getRepoIssues = function (repo) {
-    var latLongURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + 'userinput' + '&limit=5&appid=7f40902288d0d134017e794484869282';
 
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + 'lat' + '&lon=' + 'lon' + '&exclude=hourly,daily&appid=7f40902288d0d134017e794484869282';
-
-    fetch(latLongURL).then
-    if (response.ok) {
+var twoCallApi = function(lat, lon) {
+    var twoCallApi = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=de124d2c270fb53cf6dfdf88be98ad15";
+    fetch(twoCallApi).then(function(response) {
         return response.json();
-    } else {
-        throw new Error("NETWORK RESPONSE ERROR")
-    };
+    })
+}
 
-    fetch(apiUrl).then
-    if (response.ok) {
+// function saveHistory() {
+
+// }
+
+var oneCallApi = function(citynamehere) {
+    console.log(citynamehere);
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + (citynamehere) + '&appid=de124d2c270fb53cf6dfdf88be98ad15&units=imperial';
+        fetch(apiUrl).then(function(response) {
         return response.json();
-    } else {
-        throw new Error("NETWORK RESPONSE ERROR");
-    }
-}; 
+    })
+    .then(function(data) {
+        console.log(data);
+        console.log(data.main.temp);
+        console.log(data.wind.speed);
+        console.log(data.main.humidity);
+        t = data.main.temp;
+        w = data.wind.speed;
+        h = data.main.humidity;
+        temp.textContent = "Temperature: " + t + " °F";
+        wind.textContent = "Wind: " + w + " MPH";
+        humidity.textContent = "Humidity: " + h + "%";
+        console.log(cityName);
+        var dateString = moment.unix(data.dt).format("MM/DD/YYYY");        
+        cityName.textContent = data.name + " " + dateString;
+
+        let exampleElement = document.createElement("")
+        exampleElement.textContent = "hello"
+        forcast.appendChild(exampleElement)
+
+        localStorage.setItem('lat' , data.coord.lat)
+        localStorage.setItem('lon' , data.coord.lon)
+        localStorage.setItem('Most Recent Search' , data.name)
+
+    })
+
+}
+// ==================================
+
+// var getCoordinates = function(cityName) {
+//     var latLongURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=de124d2c270fb53cf6dfdf88be98ad15';
+
+//     fetch(latLongURL).then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data)
+//         console.log(data[0].lat)
+//         console.log(data[0].lon)
+//         console.log(data[0].name)
+//         oneCallApi(data[0].lat, (data[0].lon))
+//         cityNames.textContent = oneCallAPI(data[0].name)
+//     })
+// }; 
+
+// var oneCallApi = function(lat, lon) {
+//     console.log(lat)
+//     console.log(lon)
+//     var apiUrl = 'http://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,minutely,alerts&appid=de124d2c270fb53cf6dfdf88be98ad15';
+//     fetch(apiUrl).then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(data) {
+//         console.log(data)
+//     })
+// }
 
